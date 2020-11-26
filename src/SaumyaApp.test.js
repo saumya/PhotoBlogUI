f0567d2c8d2e5c56b1a8baca8d23a0a509d352d4
saumya <2saumyaray@gmail.com>
2020-11-27 00:29:23 +0530

    Modifying the Unit test cases

--- src/SaumyaApp.test.js
+++ src/SaumyaApp.test.js
@@ -3,6 +3,6 @@ import SaumyaApp from './SaumyaApp'
 
 test('<SaumyaApp> Rendering', () => {
     render(<SaumyaApp />);
-    const subtitleElement = screen.getByText(/The photo life/i);
-    expect(subtitleElement).toBeInTheDocument();
+    const titleElement = screen.getByText(/Photos/i);
+    expect(titleElement).toBeInTheDocument();
 })
